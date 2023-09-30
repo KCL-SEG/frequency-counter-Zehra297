9ba07415bd9a6048faf19d5607148e40fa14fa2e
Zehra Hasan <zehrahasan157@hotmail.com>
2023-09-30 16:23:46 +0100

    add functions to calculate item frequency
    add one function to convert the items in the list to strings
    add anther function to count the frequency of each item
    add item- frequency pair to dictionary

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,25 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def itemToStr(itemsList):
+    for i in range (0, len(itemsList)):
+        strItem = str(itemsList[i])
+        itemsList[i] = strItem
+    return itemsList
+
+def itemCounter(item, itemList):
+    count = 0
+    for i in range (0, len(itemList)):
+        if item == itemList[i]:
+            count += 1
+    return count
+
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items = itemToStr(items)
+    for i in range(0, len(items)):
+        key = items[i]
+        val = itemCounter(items[i], items)
+        frequencies[key] = val
     return frequencies
